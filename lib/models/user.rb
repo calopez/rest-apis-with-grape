a5706f36c8c7a187bb5fad0cda456d9ccc4dcc25
José Mota <jose@josemota.net>
2014-02-04 19:28:12 +0000

    Set relationships between users and assets.

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,7 +1,13 @@
 class User < Sequel::Model
   attr_writer :password
+  one_to_many :assets
+
   def before_save
     self.encrypted_password = Digest::SHA1.hexdigest @password
     super
+  end
+
+  def authorized? password
+    Digest::SHA1.hexdigest(password) == self.encrypted_password
   end
 end
