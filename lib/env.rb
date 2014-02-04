c0a51f11b6932d177850240074a05a4733ddada2
José Mota <jose@josemota.net>
2014-02-04 14:56:37 +0000

    Fix root folder and add cache_dir option for cached uploads.

--- lib/env.rb
+++ lib/env.rb
@@ -1,5 +1,6 @@
 DB = Sequel.connect "sqlite://db/db.sqlite3"
 
 CarrierWave.configure do |config|
-  config.root = File.expand_path "../../public", __FILE__
+  config.root = File.expand_path "../../", __FILE__
+  config.cache_dir = File.expand_path "../../public", __FILE__
 end
