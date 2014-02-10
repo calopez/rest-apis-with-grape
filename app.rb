84fff178e3b5ef491171d4857631686bf892b17c
José Mota <jose@josemota.net>
2014-02-10 13:21:09 +0000

    Include user class in app.

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ Bundler.require :production
 require "lib/env"
 require "lib/asset_uploader"
 require "lib/models/asset"
+require "lib/models/user"
 require "lib/api/v1"
 require "lib/api/v2"
 require "lib/api/base"
